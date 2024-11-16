368f5fd7275c10bc899729410eb2c1868eed3a67
CryptoCoin <mmmplutogt@gmail.com>
2024-11-15 21:44:57 -0500

    Update NFT.sol

--- General/NFT.sol
+++ General/NFT.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyCryptoCoinNFT is ERC721URIStorage, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("CryptoCoin NFT", "CCO") {
+        tokenCounter = 0;
+    }
+
+    function createCryptoCoinNFT(string memory tokenURI) public onlyOwner {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter++;
+    }
+}
 
